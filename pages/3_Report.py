089f31a31915771c31e310994ccf1f8cb1919306
criscarlo.dapitanon <criscarlo.dapitanon@legalmatch.com>
2024-01-19 00:31:47 +0800

    Update report logs

--- pages/3_Report.py
+++ pages/3_Report.py
@@ -1,4 +1,5 @@
 import streamlit as st 
+import pandas as pd
 from Home import face_rec
 st.set_page_config(page_title='Reporting',layout='wide')
 st.subheader('Reporting')
@@ -12,7 +13,7 @@ def load_logs(name,end=-1):
     return logs_list
 
 # tabs to show the info
-tab1, tab2 = st.tabs(['Registered Data','Logs'])
+tab1, tab2, tab3 = st.tabs(['Registered Data','Logs', 'Attendance Report'])
 
 with tab1:
     if st.button('Refresh Data'):
@@ -24,5 +25,72 @@ with tab1:
 with tab2:
     if st.button('Refresh Logs'):
         st.write(load_logs(name=name))
-        
 
+with tab3:
+    st.subheader('Attendance Report')
+    
+    # Load logs into attribute logs_list
+    logs_list = load_logs(name=name)
+
+    # Step 1: Convert the logs that in list of bytes into list of string
+    convert_byte_to_string = lambda x: x.decode('utf-8')
+    logs_list_string = list(map(convert_byte_to_string, logs_list))
+
+    # Step 2: Split string by @ and create nested list
+    split_string = lambda x: x.split('@')
+    logs_nested_list = list(map(split_string, logs_list_string))
+
+    # Convert nested list info into dataframe
+    logs_df = pd.DataFrame(logs_nested_list, columns= ['Name','Role','Timestamp'])
+
+# Step 3: Time base Analysis or Report Time-In Time-out
+    logs_df['Timestamp'] = pd.to_datetime(logs_df['Timestamp'])
+    logs_df['Date'] = logs_df['Timestamp'].dt.date
+
+# Step 3.1: Calculate Report Time-In Time-out
+# In Time: At which person is first detected in that day (min Timestamp of the date)
+# Out Time: At which person is last detected in that day (max Timestamp of the date)
+
+    report_df = logs_df.groupby(by=['Date','Name','Role']).agg(
+        In_time = pd.NamedAgg('Timestamp','min'), # In Time
+        Out_time = pd.NamedAgg('Timestamp','max') # Out Time
+    ).reset_index()
+
+    report_df['In_time'] = pd.to_datetime(report_df['In_time'])
+    report_df['Out_time'] = pd.to_datetime(report_df['Out_time'])
+
+    report_df['Duration'] = report_df['Out_time'] - report_df['In_time']
+
+# Step 4: Marking Person is Present or Absent
+    all_dates = report_df['Date'].unique()
+    name_role = report_df[['Name','Role']].drop_duplicates().values.tolist()
+
+    date_name_rol_zip_df = []
+    for dt in all_dates:
+        for name, role in name_role:
+            date_name_rol_zip_df.append([dt, name, role])
+
+    date_name_rol_zip_df = pd.DataFrame(date_name_rol_zip_df, columns=['Date','Name','Role'])
+
+    # lef join with report_df
+    date_name_rol_zip_df = pd.merge(date_name_rol_zip_df, report_df, how='left',on=['Date','Name','Role'])
+
+    # Duration
+    # Hours
+    date_name_rol_zip_df['Duration_seconds'] = date_name_rol_zip_df['Duration'].dt.seconds
+    date_name_rol_zip_df['Duration_hours'] = date_name_rol_zip_df['Duration_seconds'] / (60*60)
+
+    def status_marker(x):
+
+        if pd.Series(x).isnull().all():
+            return 'Absent'
+        
+        elif x >= 0 and x < 1:
+            return 'Late'
+        
+        elif x >= 2 and x <= 10:
+            return 'Present'
+        
+    date_name_rol_zip_df['Status'] = date_name_rol_zip_df['Duration_hours'].apply(status_marker)
+
+    st.dataframe(date_name_rol_zip_df,column_order=['Date','Name','Role','In_time','Out_time','Status'])
